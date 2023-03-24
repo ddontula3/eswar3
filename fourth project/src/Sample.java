b5b8df370e8b43a853e821685aa3293a53881d0b
ddontula3 <ddontula3@gmail.com>
2023-03-24 16:56:10 +0530

    this is my third commit

--- fourth project/src/Sample.java
+++ fourth project/src/Sample.java
@@ -2,10 +2,12 @@
 public class Sample {
 
 	public static void main(String[] args) {
-		int a,b,c;
+		int a,b,c,d;
 		a=10;b=30;
 		c=a+b;
+		d=a*b;
 		System.out.println(c);
+		System.out.println(d);
 
 	}
 
